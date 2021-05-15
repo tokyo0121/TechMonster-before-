081d3e32ebc871bfb28ddf341e9bf04fe60c0daa
Saki Igeta <p19026is@gm.tsuda.ac.jp>
2021-05-15 19:05:12 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 井ケ田　沙紀 on 2021/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
